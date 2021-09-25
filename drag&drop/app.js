546cfd0bc8884c505ec21da2943b9fe5362b7bea
chicoumbetov <shyngysmaaan@gmail.com>
2021-09-25 23:45:24 +0200

    Detecting item by placeholder when it's over it

--- drag&drop/app.js
+++ drag&drop/app.js
@@ -1,7 +1,15 @@
 const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
 
 item.addEventListener('dragstart', dragStart)
 item.addEventListener('dragend', dragEnd)
+
+for (const placeholder of placeholders) {
+    placeholder.addEventListener('dragover', dragover)
+    placeholder.addEventListener('dragenter', dragenter)
+    placeholder.addEventListener('dragleave', dragleave)
+    placeholder.addEventListener('drop', drop)
+}
 
 function dragStart(event) {
     // console.log('dragStart', event.target)
@@ -14,4 +22,21 @@ function dragEnd(event) {
     event.target.classList.remove('hold', 'hide')
     // or another way that give same result
     // event.target.className('item')
+}
+
+function dragover(event) {
+    console.log('dragover') 
+}
+
+function dragenter(event) {
+    console.log('dragenter')
+    event.target.classList.add('hovered') 
+}
+
+function dragleave(event) {
+    console.log('dragleave')
+    event.target.classList.remove('hovered')  
+}
+function drop(event) {
+    console.log('drop') 
 }
