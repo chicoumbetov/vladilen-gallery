660da02abc6b8e912d87e202ceecae11b7d2c5e8
chicoumbetov <shyngysmaaan@gmail.com>
2021-09-25 23:32:42 +0200

    Make better draging of item

--- drag&drop/app.js
+++ drag&drop/app.js
@@ -4,11 +4,14 @@ item.addEventListener('dragstart', dragStart)
 item.addEventListener('dragend', dragEnd)
 
 function dragStart(event) {
-    console.log('dragStart', event.target)
+    // console.log('dragStart', event.target)
     event.target.classList.add('hold')
+    setTimeout(() => { event.target.classList.add('hide')}, 0)
 }
 
 function dragEnd(event) {
-    console.log('dragEnd', event.target)   
-    event.target.classList.remove('hold')
+    // console.log('dragEnd', event.target)   
+    event.target.classList.remove('hold', 'hide')
+    // or another way that give same result
+    // event.target.className('item')
 }
