36e797da68f9db7a91c098e5656040755fb9876b
chicoumbetov <shyngysmaaan@gmail.com>
2021-09-25 22:51:37 +0200

    Pluginize functions

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const slides = document.querySelectorAll(".slide")
+const slidesPlugin = (activeSlide = 0) => {
+    const slides = document.querySelectorAll(".slide")
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClass()
+    slides[activeSlide].classList.add('active')
 
-        slide.classList.add('active')
-    })
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClass()
+
+            slide.classList.add('active')
+        })
+    }
+
+    function clearActiveClass() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
+
 }
 
-function clearActiveClass() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
-}
+slidesPlugin()
